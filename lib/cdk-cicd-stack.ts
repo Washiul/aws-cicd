fc77d157762d26a64c1f67c6e5f6a9afe006f520
Md Washi Ul Hoque <washi.washi786@gmail.com>
2023-06-25 16:59:05 +0530

    Adding test stage with lamda stack

--- lib/cdk-cicd-stack.ts
+++ lib/cdk-cicd-stack.ts
@@ -1,9 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './pipeline-stage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const pipeline = new CodePipeline(this, 'Awesome Pipeline Stack', {
+      pipelineName: 'AwesomePipeline',
+      synth: new ShellStep('Synth',{
+        input: CodePipelineSource.gitHub('Washiul/aws-cicd', 'main'),
+        commands: [
+          'npm ci',
+          'npx cdk synth'
+        ]
+      })
+    });
 
+    pipeline.addStage(new PipelineStage(this, 'pipelineTestStage', {
+      stageName:'test'
+    }));
   }
 }
---
+++ lib/lamda-stack.ts
@@ -0,0 +1,11 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+interface LamdaStackProps extends StackProps{
+    stageName?: string
+}
+export class LamdaStack extends Stack{
+    constructor(scope: Construct, id: string, props?: LamdaStackProps){
+        super(scope, id, props);
+    }
+}
